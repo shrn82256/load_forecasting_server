ee0bafce1443ffa230b6d5a4bcff0b066ce4df31
shrn82256 <shrn82256@gmail.com>
2019-03-29 01:36:58 +0530

    fixed device_id as string and changed function names to pep8

--- ml/main.py
+++ ml/main.py
@@ -5,29 +5,29 @@ import os
 ml_api = Blueprint('ml_api', __name__)
 
 
-@ml_api.route("/ml/train/<int:device>")
-def trainModel(device):
+@ml_api.route("/ml/train/<string:device>")
+def train_model(device):
     db = "db/{}.csv".format(device)
     if os.path.isfile(db):
-        train.main(str(device), str(db), model_architecture="single_lstm")
+        train.main(device, db)
         return "true"
     else:
         return jsonify(False)
 
 
-@ml_api.route("/ml/exec/<int:device>")
-def executeModel(device):
+@ml_api.route("/ml/exec/<string:device>")
+def execute_model(device):
     db = "db/{}.csv".format(device)
     if os.path.isfile(db):
-        return jsonify({"next": execute.main(str(device), str(db))})
+        return jsonify({"next": execute.main(device, db)})
     else:
         return jsonify(False)
 
 
-@ml_api.route("/ml/test/<int:device>")
-def testModel(device):
+@ml_api.route("/ml/test/<string:device>")
+def test_model(device):
     db = "db/{}.csv".format(device)
     if os.path.isfile(db):
-        return jsonify(test.main(str(device), str(db)))
+        return jsonify(test.main(device, db))
     else:
         return jsonify(False)
